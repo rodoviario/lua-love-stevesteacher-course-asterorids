4f2431432d29b085fe5fb83a4ab87dd50dc11870
Rodolfo Guagnini <rodolfo.guagnini@gmail.com>
2022-09-22 15:38:56 -0300

    feat: hide mouse and enable debugging

--- main.lua
+++ main.lua
@@ -2,11 +2,14 @@
 local love = require "love"
 
 function love.load()
+  love.mouse.setVisible(false)
+  _G.mouse_x, _G.mouse_y = 0, 0
 
+  local show_debugging = true
 end
 
 function love.update()
-
+  _G.mouse_x, _G.mouse_y = love.mouse.getPosition()
 end
 
 function love.draw()
