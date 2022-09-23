ac0e61fd058788942f457a1da29badf36f4b51b0
Rodolfo Guagnini <rodolfo.guagnini@gmail.com>
2022-09-23 18:01:22 -0300

    feat: add thrusters and flame effect

--- Player.lua
+++ Player.lua
@@ -18,11 +18,46 @@ function Player(debugging)
     thrust = {
       x = 0,
       y = 0,
-      speed = 5
+      speed = 5,
+      big_flame = false,
+      flame = 2.0
     },
+
+    drawFlameThrust = function (self, fillType, color)
+      love.graphics.setColor(color)
+      
+      love.graphics.polygon(
+        fillType,
+        self.x - self.radius * (2 / 3 * math.cos(self.angle) + 0.5 * math.sin(self.angle)),
+        self.y + self.radius * (2 / 3 * math.sin(self.angle) - 0.5 * math.cos(self.angle)),
+        self.x - self.radius * self.thrust.flame * math.cos(self.angle),
+        self.y + self.radius * self.thrust.flame * math.sin(self.angle),
+        self.x - self.radius * (2 / 3 * math.cos(self.angle) - 0.5 * math.sin(self.angle)),
+        self.y + self.radius * (2 / 3 * math.sin(self.angle) + 0.5 * math.cos(self.angle))
+        )
+    end,
 
     draw = function (self)
       local opacity = 1
+
+      if self.thrusting then
+        if not self.thrust.big_flame then
+          self.thrust.flame = self.thrust.flame - 1 / love.timer.getFPS()
+
+          if self.thrust.flame < 1.5 then
+            self.thrust.big_flame = true
+          end
+        else
+          self.thrust.flame = self.thrust.flame + 1 / love.timer.getFPS()
+
+          if self.thrust.flame > 2.5 then
+            self.thrust.big_flame = false
+          end
+        end
+
+        self:drawFlameThrust("fill", { 255 / 255, 102 / 255, 25 / 255 })
+        self:drawFlameThrust("line", { 1, 0.16, 0 })
+      end
 
       if debugging then
         love.graphics.setColor(1, 0, 0)
@@ -31,6 +66,7 @@ function Player(debugging)
 
         love.graphics.circle("line", self.x, self.y, self.radius)
       end
+
       love.graphics.setColor(1, 1, 1, opacity)
 
       love.graphics.polygon(
