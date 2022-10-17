42e433169fa25b995186785d0782f64d67196ac7
Rodolfo Guagnini <rodolfo.guagnini@gmail.com>
2022-10-17 19:34:57 -0300

    feat: add lunajson luarocks package

--- globals.lua
+++ globals.lua
@@ -1,3 +1,5 @@
+ local lunajson = require "lunajson"
+
 _G.ASTEROID_SIZE =  100
 _G.show_debugging = false
 _G.destroy_ast = false
@@ -11,3 +13,11 @@ end
 -- function calculateDistance(x1, y1, x2, y2)
 --   return math.sqrt(((x2 - x1) ^ 2) + ((y2 - y1) ^ 2))
 -- end
+
+function readJSON(file_name)
+  local file = io.open("src/data/" .. file_name .. ".json", "r")
+  local data = file:read("*all")
+  file:close()
+
+  return lunajson.decode(data);
+end
